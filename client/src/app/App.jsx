34570c31bae71877cf5fd2212e80f62d239da69e
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-04-06 17:23:44 +0200

    feat (Client) : Create Home Page and create Gallary in Single Studio Page

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import AppRouter from "../Routes/Router";
+import React from 'react';
 
-function App() {
-  return <AppRouter />;
+const queryClient = new QueryClient()
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* Main Project Content */}
+      <AppRouter />
+
+      {/* React Query Debugger */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
-
-export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,34 +1,13 @@
 import React from 'react'
-import { useQuery } from '@tanstack/react-query'
+import Hero from '../../components/Home/Hero/Hero'
+import Studio from '../../components/Home/Studio/Studio'
 
 const Home = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ['posts'],
-    queryFn: async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      return res.json();
-    }
-  })
-
   return (
-    <div className="container mx-auto p-4 flex-column flex flex-col items-center h-screen bg-sky-900" >
-      <h1 className="text-3xl font-bold mb-4 text-sky-300 ">Home</h1>
-
-      {isLoading ? (
-        <p className="text-lg text-gray-700 animate-pulse">Loading...</p>
-      ) : (
-        <ul className="w-full max-w-md bg-white shadow-lg rounded-lg p-4">
-          {data.map((post) => (
-            <li
-              key={post.id}
-              className="py-2 px-4 border-b last:border-none text-gray-800 hover:bg-gray-100 transition"
-            >
-              {post.name}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <Hero />
+      <Studio />
+    </>
   )
 }
 
