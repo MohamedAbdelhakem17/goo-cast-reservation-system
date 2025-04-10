701631aee3f24809f2548727d982ae044c183438
Mohamed Abdelhakem <107258224+MohamedAbdelhakem17@users.noreply.github.com>
2025-04-10 17:28:16 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-echo "⚡ Pulling latest updates..."
+echo "? Pulling latest updates..."
 git pull origin main
 
-echo "📦 Installing backend dependencies..."
+echo "?? Installing backend dependencies..."
 cd server
 npm install
 
-echo "🧱 Building frontend..."
+echo "?? Building frontend..."
 cd ../client
 npm install
+npm run build -- --base=/goocast/
 
-BUILD_OUTPUT=$(npm run build -- --base=/goocast/ 2>&1)  
-BUILD_STATUS=$?
-
-if [ $BUILD_STATUS -ne 0 ]; then
-    echo -e "🚨 Frontend build failed!\n\nError Details:\n$BUILD_OUTPUT" | mail -s "Build Error: Frontend Build Failed" m.abdelhakem@dottopia.com
-    exit 1
-else
-    echo "✅ Frontend build succeeded!" | mail -s "Build Success: Frontend Build Succeeded" m.abdelhakem@dottopia.com
-fi
-
-echo "🚀 Restarting backend..."
+echo "?? Restarting backend..."
 
 pm2 restart goocast
