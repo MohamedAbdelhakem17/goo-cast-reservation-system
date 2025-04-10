2a0d0d94d92cd3bfe058ec8f8290c90c82270fd1
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-04-10 16:17:37 +0200

    Feat (Client) : Create Date Picker UI

--- client/src/components/Booking/Select-Additional-Services/SelectAdditionalServices.jsx
+++ client/src/components/Booking/Select-Additional-Services/SelectAdditionalServices.jsx
@@ -11,35 +11,12 @@ export default function SelectAdditionalServices() {
                     stiffness: 260,
                     damping: 20,
                 }}
-                className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mx-auto"
+                className=" bg-main/10 rounded-full flex items-center justify-center mx-auto p-6"
             >
-                <svg
-                    className="w-8 h-8 text-green-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M5 13l4 4L19 7"
-                    />
-                </svg>
+                <h2 className='text-2xl text-main font-bold'>Select Additional Services</h2>
+
             </motion.div>
-            <h3 className="text-xl font-bold text-gray-800">
-                Booking Confirmed!
-            </h3>
-            <p className="text-gray-600">
-                Thank you for your booking. We've sent a confirmation
-                email with all the details.
-            </p>
-            <div className="bg-blue-50 p-4 rounded-md border border-blue-100 mt-4">
-                <p className="text-blue-800 font-medium">
-                    Booking Reference: #BK12345
-                </p>
-            </div>
         </div>
     )
 }
+
---
+++ client/src/components/Booking/Select-Date-Time/Calendar/Calendar.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./datePicker.css";
+
+export default function Calendar() {
+    const [startDate, setStartDate] = useState(new Date());
+
+    const disableDate = new Date(2025, 3, 25);
+
+    const isDateDisabled = (date) => {
+        return date.getDate() === disableDate.getDate() &&
+            date.getMonth() === disableDate.getMonth() &&
+            date.getFullYear() === disableDate.getFullYear();
+    };
+
+    return (
+        <div className="border-b-1 border-t-1 border-gray-300 rounded-lg p-4">
+            <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="dd/MM/yyyy"
+                calendarClassName="w-full"
+                inline
+                minDate={new Date()}
+                filterDate={(date) => !isDateDisabled(date)}
+            />
+        </div>
+    )
+}
