c52cef41649b0a18a0a2d5da2bd03d672cf9d7e0
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-04-13 19:15:02 +0200

    edit

--- client/src/components/Booking/Personal-Information/PersonalInformation.jsx
+++ client/src/components/Booking/Personal-Information/PersonalInformation.jsx
@@ -5,7 +5,9 @@ import { useBooking } from '../../../context/Booking-Context/BookingContext';
 
 export default function PersonalInformation() {
     const inputRef = useRef(null);
-    const { getField, getBookingError  , formik} = useBooking();
+    const { getField, getBookingError, formik, bookingData  ,setBookingField} = useBooking();
+
+    const { fullName, phone, email, brand } = bookingData.personalInfo
 
     useEffect(() => {
         if (inputRef.current) {
@@ -28,12 +30,14 @@ export default function PersonalInformation() {
                     type="text"
                     id="email"
                     label="Email"
-                    inputRef={inputRef}
+                    // inputRef={inputRef}
                     placeholder="Enter your Email"
                     {...getField("personalInfo.email")}
                     errors={getBookingError('personalInfo.email')}
                     onBlur={getField('personalInfo.email').onBlur}
+                    onChange={(e) => setBookingField('personalInfo.email', e.target.value)}
                     touched={formik.touched.email}
+                    value={email}
                 />
             </motion.div>
 
@@ -45,7 +49,9 @@ export default function PersonalInformation() {
                     placeholder="Enter your Name"
                     errors={getBookingError('personalInfo.fullName')}
                     onBlur={getField('personalInfo.fullName').onBlur}
+                    onChange={(e) => setBookingField('personalInfo.fullName', e.target.value)}
                     touched={formik.touched.name}
+                    value={fullName}
                 />
             </motion.div>
 
@@ -54,9 +60,11 @@ export default function PersonalInformation() {
                     type="text"
                     id="brandName"
                     label="Your Brand Name"
+                    value={brand}
                     placeholder="Enter your Brand Name"
                     {...getField("personalInfo.brand")}
                     errors={getBookingError('personalInfo.brand')}
+                    onChange={(e) => setBookingField('personalInfo.brand', e.target.value)}
                     onBlur={getField('personalInfo.brand').onBlur}
                     touched={formik.touched.brandName}
                 />
@@ -66,12 +74,14 @@ export default function PersonalInformation() {
                 <Input
                     type="text"
                     id="phoneNumber"
+                    value={phone}
                     label="Phone Number"
                     placeholder="Enter your Phone Number"
                     {...getField("personalInfo.phone")}
                     errors={getBookingError('personalInfo.phone')}
                     onBlur={getField('personalInfo.phone').onBlur}
                     touched={formik.touched.phoneNumber}
+                    onChange={(e) => setBookingField('personalInfo.phone', e.target.value)}
                 />
             </motion.div>
         </form>
