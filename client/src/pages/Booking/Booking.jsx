40501e13c881c045cc167c3d6cbb4b73bef8f7bf
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-05-05 17:36:43 +0300

    Booking confirmation

--- client/src/pages/Booking/Booking.jsx
+++ client/src/pages/Booking/Booking.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import StepIndicator from "../../components/Booking/Step-Indicator/StepIndicator";
 import NavigationButtons from "../../components/Booking/Navigation-Buttons/NavigationButtons";
@@ -8,10 +8,20 @@ import SelectAdditionalServices from "../../components/Booking/Select-Additional
 import PersonalInformation from "../../components/Booking/Personal-Information/PersonalInformation";
 import { useBooking } from "../../context/Booking-Context/BookingContext";
 import Cart from "../../components/Booking/Cart/Cart";
+import ConfirmationBooking from "../../components/Booking/Confirmation-Booking/ConfirmationBooking";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Booking() {
-  const { currentStep } = useBooking();
+  const { currentStep, bookingData } = useBooking();
+  const navigate = useNavigate();
+  const location = useLocation();
   const [showMobileCart, setShowMobileCart] = useState(false);
+  const [confirmation, setConfirmation] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    setConfirmation(params.get("step") === "confirmation");
+  }, [location.search]);
 
   const containerVariants = {
     hidden: { opacity: 0 },
@@ -61,100 +71,108 @@ export default function Booking() {
     }),
   };
 
+  const handelGoToConfirmation = () => {
+    setConfirmation(true);
+    navigate(`/booking?step=confirmation`);
+  };
+
   return (
     <div className="py-12 lg:px-8">
-      {/* Step Indicator */}
-      <StepIndicator />
+      {confirmation ? (
+        <ConfirmationBooking booking={bookingData} />
+      ) : (
+        <>
+          {/* Step Indicator */}
+          <StepIndicator />
 
-      {/* Mobile Cart Button */}
-      {currentStep === 3 && (
-        <div className="lg:hidden text-right px-4 my-1">
-          <button
-            onClick={() => setShowMobileCart(true)}
-            className="bg-primary text-main px-4 py-2 rounded-md shadow-md"
-          >
-            <i className="fa-solid fa-cart-shopping text-xl"></i>
-          </button>
-        </div>
-      )}
-
-      {/* Main Content */}
-      <motion.div
-        className="my-4 bg-white rounded-xl shadow-lg overflow-hidden"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <AnimatePresence mode="wait" custom={currentStep}>
-          <motion.div
-            key={currentStep}
-            custom={currentStep}
-            variants={slideVariants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            className="md:px-6 py-8"
-          >
-            <motion.div
-              variants={containerVariants}
-              initial="hidden"
-              animate="visible"
-              exit="exit"
-              className="space-y-6 lg:space-y-0 lg:flex lg:gap-6 items-start"
-            >
-              {/* Main Step Content */}
-              <motion.div
-                variants={itemVariants}
-                className="md:p-6 rounded-lg flex-1"
-              >
-                {currentStep === 1 && <SelectStudio />}
-                {currentStep === 2 && <SelectDateTime />}
-                {currentStep === 3 && <SelectAdditionalServices />}
-                {currentStep === 4 && <PersonalInformation />}
-              </motion.div>
-
-              {/* Cart Sidebar on Large Screens */}
-              {currentStep === 3 && (
-                <motion.div
-                  variants={itemVariants}
-                  className="md:px-2 md:py-6 rounded-lg w-full lg:w-1/3 hidden lg:block bg-gray-50 shadow-md sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto"
-                >
-                  <Cart />
-                  {/* Add your cart component here */}
-                </motion.div>
-              )}
-            </motion.div>
-          </motion.div>
-        </AnimatePresence>
-      </motion.div>
-
-      {/* Navigation Buttons */}
-      <NavigationButtons />
-
-      {/* Mobile Cart Modal */}
-      <AnimatePresence>
-        {showMobileCart && (
-          <motion.div
-            initial={{ y: "100%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "100%" }}
-            transition={{ type: "spring", stiffness: 200, damping: 25 }}
-            className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t-2 border-main/50 rounded-t-xl shadow-xl p-6 overflow-auto max-h-[400px]"
-          >
-            <div className="flex justify-between items-center mb-4">
-              <h2 className="text-lg font-semibold">Cart</h2>
+          {/* Mobile Cart Button */}
+          {currentStep === 3 && (
+            <div className="lg:hidden text-right px-4 my-1">
               <button
-                onClick={() => setShowMobileCart(false)}
-                className="text-gray-500 hover:text-gray-700"
+                onClick={() => setShowMobileCart(true)}
+                className="bg-primary text-main px-4 py-2 rounded-md shadow-md"
               >
-                <i className="fa-solid fa-xmark text-2xl text-main"></i>
+                <i className="fa-solid fa-cart-shopping text-xl"></i>
               </button>
             </div>
-            {/* Add your cart component here */}
-            <Cart />
+          )}
+
+          {/* Main Content */}
+          <motion.div
+            className="my-4 bg-white rounded-xl shadow-lg overflow-hidden"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <AnimatePresence mode="wait" custom={currentStep}>
+              <motion.div
+                key={currentStep}
+                custom={currentStep}
+                variants={slideVariants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                className="md:px-6 py-8"
+              >
+                <motion.div
+                  variants={containerVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  className="space-y-6 lg:space-y-0 lg:flex lg:gap-6 items-start"
+                >
+                  <motion.div
+                    variants={itemVariants}
+                    className="md:p-6 rounded-lg flex-1"
+                  >
+                    {currentStep === 1 && <SelectStudio />}
+                    {currentStep === 2 && <SelectDateTime />}
+                    {currentStep === 3 && <SelectAdditionalServices />}
+                    {currentStep === 4 && <PersonalInformation />}
+                  </motion.div>
+
+                  {/* Cart Sidebar on Large Screens */}
+                  {currentStep === 3 && (
+                    <motion.div
+                      variants={itemVariants}
+                      className="md:px-2 md:py-6 rounded-lg w-full lg:w-1/3 hidden lg:block bg-gray-50 shadow-md sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto"
+                    >
+                      <Cart />
+                    </motion.div>
+                  )}
+                </motion.div>
+              </motion.div>
+            </AnimatePresence>
           </motion.div>
-        )}
-      </AnimatePresence>
+
+          {/* Navigation Buttons */}
+          <NavigationButtons handelGoToConfirmation={handelGoToConfirmation} />
+
+          {/* Mobile Cart Modal */}
+          <AnimatePresence>
+            {showMobileCart && (
+              <motion.div
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "100%" }}
+                transition={{ type: "spring", stiffness: 200, damping: 25 }}
+                className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t-2 border-main/50 rounded-t-xl shadow-xl p-6 overflow-auto max-h-[400px]"
+              >
+                <div className="flex justify-between items-center mb-4">
+                  <h2 className="text-lg font-semibold">Cart</h2>
+                  <button
+                    onClick={() => setShowMobileCart(false)}
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    <i className="fa-solid fa-xmark text-2xl text-main"></i>
+                  </button>
+                </div>
+                <Cart />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </>
+      )}
     </div>
   );
 }
