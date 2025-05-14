6c0de447dc91ea97a71914e30cb54975f5f1eb12
Mohamed Abdelhakem <mohamed.abdelgakem3200@gimal.com>
2025-05-14 13:35:32 +0300

    admin mange workspace

--- server/src/routes/user-route/user-route.js
+++ server/src/routes/user-route/user-route.js
@@ -7,14 +7,24 @@ const protectRoute = require("../../middleware/protect.middleware");
 const allowTo = require("../../middleware/allow-to-middleware");
 const { USER_ROLE } = require("../../config/system-variables");
 
-router.use(protectRoute, allowTo(USER_ROLE.USER));
+router.use(protectRoute);
 
 router
   .route("/")
-  .get(userController.getUserData)
-  .put(userController.updateUserData);
+  .get(allowTo(USER_ROLE.USER), userController.getUserData)
+  .put(allowTo(USER_ROLE.USER), userController.updateUserData);
+  
 
-router.get("/user-stats", userController.getUserStats);
-router.put("/edit-password", userController.editLoginUserPassword);
+router.get("/user-stats", allowTo(USER_ROLE.USER), userController.getUserStats);
+router.put(
+  "/edit-password",
+  allowTo(USER_ROLE.USER),
+  userController.editLoginUserPassword
+);
+
+router.route("/all").get(allowTo(USER_ROLE.ADMIN), userController.getAllUsers);
+router
+  .route("/workspace-mange")
+  .post(allowTo(USER_ROLE.ADMIN), userController.manageWorkSpace);
 
 module.exports = router;
