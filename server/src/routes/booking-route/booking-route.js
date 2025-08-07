04c4435f61f900e3603d92669b2bb2b3449779e4
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2025-08-07 11:45:26 +0300

    create route fro booking with GHL

--- server/src/routes/booking-route/booking-route.js
+++ server/src/routes/booking-route/booking-route.js
@@ -7,18 +7,35 @@ const { USER_ROLE } = require("../../config/system-variables");
 const bookingController = require("../../controller/booking-controller/booking-controller");
 
 // get Fully booked dates for a studio
-router.route("/fully-booked/:studioId/").get(bookingController.getFullyBookedDates);
-router.route("/available-studios/:date/:categoryId").get(bookingController.getAvailableStudios)
+router
+  .route("/fully-booked/:studioId/")
+  .get(bookingController.getFullyBookedDates);
+
+router
+  .route("/available-studios/:date/:categoryId")
+  .get(bookingController.getAvailableStudios);
+
 router.route("/available-slots").post(bookingController.getAvailableStartSlots);
-router.route("/available-end-slots").post(bookingController.getAvailableEndSlots);
+
+router
+  .route("/available-end-slots")
+  .post(bookingController.getAvailableEndSlots);
+
 router.route("/").post(bookingController.createBooking);
+router.route("/create/ghl").post(bookingController.ghlCreateBooking);
 
-router.use(protectRoute)
-router.route("/").get( allowTo(USER_ROLE.ADMIN),bookingController.getAllBookings);
-router.route("/:id").put( allowTo(USER_ROLE.ADMIN) , bookingController.changeBookingStatus);
+router.use(protectRoute);
 
-router.route("/user-bookings").get(allowTo(USER_ROLE.USER),bookingController.getUserBookings);
+router
+  .route("/")
+  .get(allowTo(USER_ROLE.ADMIN), bookingController.getAllBookings);
 
+router
+  .route("/:id")
+  .put(allowTo(USER_ROLE.ADMIN), bookingController.changeBookingStatus);
 
-module.exports = router; 
+router
+  .route("/user-bookings")
+  .get(allowTo(USER_ROLE.USER), bookingController.getUserBookings);
 
+module.exports = router;
