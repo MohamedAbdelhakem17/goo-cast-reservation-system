6e2dea39a1e9e1b6fb177b1286ed34cbf37d0a21
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2025-08-10 13:39:49 +0300

    Handel Add GTM

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './assets/css/index.css'
 import App from './app/App.jsx'
+import TagManager from 'react-gtm-module';
 
 
+  // Initialize GTM with your GTM ID
+  const tagManagerArgs = {
+    gtmId: "GTM-P92D4BCV",
+  };
+
+  TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
