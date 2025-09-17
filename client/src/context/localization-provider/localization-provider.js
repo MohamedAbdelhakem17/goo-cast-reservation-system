88a7d0c18dfd13237c7a8c71a1c1802edb5c0e5d
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2025-09-17 12:18:27 +0300

    start localization

---
+++ client/src/context/localization-provider/localization-provider.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function LocalizationProvider({ children }) {
+  return <div>LocalizationProvider</div>;
+}
