63328a6a1f04b596520a92c3a9194ebc7ee1a2eb
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2025-10-01 10:55:33 +0300

    fix: change default server port from 5000 to 7000

--- server/src/app.js
+++ server/src/app.js
@@ -129,7 +129,7 @@ app.use("*", (req, res, next) => {
 app.use(errorMiddlewareHandler);
 
 // ====== Start Server ======
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 const server = app.listen(PORT, () => {
   console.log(
     `🚀 Server running on port: ${PORT} in ${process.env.ENVIRONMENT_MODE} mode`
