47f226e29bed43c97f18087c5f65f2b1e91cbb19
Mohamed Abdelhakem <107258224+MohamedAbdelhakem17@users.noreply.github.com>
2025-10-01 11:10:04 +0300

    Update main-layout.jsx

--- client/src/layout/main-layout/main-layout.jsx
+++ client/src/layout/main-layout/main-layout.jsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD:client/src/components/layout/Main-Layout/MainLayout.jsx
-import { Outlet, useLocation } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
-import PageTracker from "../../PageTracker/PageTracker";
-// import WhatsAppButton from "./../Footer/whats-app-button";
-
-const MainLayout = () => {
-  const location = useLocation();
-
-  return (
-    <>
-      {/* Page Tracker */}
-
-      <PageTracker />
-
-      {/* Main Layout */}
-      <div className="flex flex-col min-h-screen">
-        {/* Navbar */}
-        {location.pathname !== "/booking" && <Navbar />}
-=======
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '@/layout/_components/navbar/navbar';
 import Footer from '@/layout/_components/footer/footer';
@@ -32,26 +11,13 @@ const MainLayout = () => {
         <>
             {/* Main Layout */}
             <div className="flex flex-col min-h-screen">
->>>>>>> d6e0283572ca03308adfcb2e44a14af7d4499395:client/src/layout/main-layout/main-layout.jsx
 
-        {/* Main Content */}
-        <main
-          className={`flex flex-col ${
-            location.pathname !== "/booking" ? "pt-16" : ""
-          } flex-1`}
-        >
-          <Outlet />
-        </main>
+                {/* Navbar */}
+                {
+                    location.pathname !== "/booking" && <Navbar />
+                }
 
-        {/* Footer */}
-        {location.pathname !== "/booking" && <Footer />}
 
-<<<<<<< HEAD:client/src/components/layout/Main-Layout/MainLayout.jsx
-        {/* <WhatsAppButton /> */}
-      </div>
-    </>
-  );
-=======
                 {/* Main Content */}
                 <main className={`flex flex-col ${location.pathname !== "/booking" ? "pt-16" : ""} flex-1`}>
                     <Outlet />
@@ -62,7 +28,6 @@ const MainLayout = () => {
             </div>
         </>
     );
->>>>>>> d6e0283572ca03308adfcb2e44a14af7d4499395:client/src/layout/main-layout/main-layout.jsx
 };
 
 export default MainLayout;
