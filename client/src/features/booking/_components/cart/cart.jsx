02e7f6759daec1ab9f13a68a6c46a044989e8ab6
= <=>
2025-10-16 16:10:30 +0300

    fix(cart): fix overlay  responsive design for cart component with media query

--- client/src/features/booking/_components/cart/cart.jsx
+++ client/src/features/booking/_components/cart/cart.jsx
@@ -2,6 +2,7 @@ import { useBooking } from "@/context/Booking-Context/BookingContext";
 import CartContent from "./_components/cart-content";
 import { Loading } from "@/components/common";
 import Sticky from "react-sticky-el";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 export default function Cart() {
   const { bookingData } = useBooking();
@@ -10,23 +11,29 @@ export default function Cart() {
     return <Loading />;
   }
 
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   return (
     <>
       {/*  Cart */}
       <div className="block">
-        <Sticky
-          topOffset={-150}
-          bottomOffset={140}
-          stickyStyle={{
-            top: "190px",
-            zIndex: 40,
-            transition: "top 0.3s ease-in-out",
-          }}
-          boundaryElement="#cart-wrapper"
-          hideOnBoundaryHit={true}
-        >
+        {isDesktop ? (
+          <Sticky
+            topOffset={-150}
+            bottomOffset={140}
+            stickyStyle={{
+              top: "190px",
+              zIndex: 40,
+              transition: "top 0.3s ease-in-out",
+            }}
+            boundaryElement="#cart-wrapper"
+            hideOnBoundaryHit={true}
+          >
+            <CartContent />
+          </Sticky>
+        ) : (
           <CartContent />
-        </Sticky>
+        )}
       </div>
     </>
   );
