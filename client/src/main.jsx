3a0f79cba178e5efaf4e07536937bd6e7e923d18
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2025-12-24 15:57:24 +0200

    deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../i18n/config.js";
 import App from "./App.jsx";
-// import "./config/vite-google-tag-manager.js";
+import "./config/vite-google-tag-manager.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
