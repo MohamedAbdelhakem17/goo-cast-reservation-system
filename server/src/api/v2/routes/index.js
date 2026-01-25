25fc94771bccafee2d5bf21be461f6a0fdab3f39
= <=>
2026-01-25 07:55:01 +0200

    refactor(routes): clean up formatting and structure in index.js

--- server/src/api/v2/routes/index.js
+++ server/src/api/v2/routes/index.js
@@ -1,12 +1,12 @@
 const routes = [
-
+  {
+    path: "api/v2/bookings",
+    router: require("./booking-route/booking-route-v2"),
+  },
 ];
 
 const AmountRoutesV2 = (app) => {
   routes.forEach(({ path, router }) => app.use(path, router));
-
-
 };
 
 module.exports = AmountRoutesV2;
-
