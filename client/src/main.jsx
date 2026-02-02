ad0bb4d0951ddb63a3d6f6acafd92caffa471bb4
Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2026-02-02 15:02:38 +0200

    Refactor API base URL configuration to dynamically select based on environment and update Google Tag Manager import logic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../i18n/config.js";
 import App from "./App.jsx";
-// import "./config/vite-google-tag-manager.js";
 import "./index.css";
+// Dynamically import Google Tag Manager based on environment
+if (import.meta.env.VITE_ENV === "production") {
+  import("./config/vite-google-tag-manager.js");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
